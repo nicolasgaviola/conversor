86501b57fcc9b81913964bc8dbf3395d6d4ccc65
Nicolas Gaviola <nicolasgaviola@gmail.com>
2021-07-16 17:50:25 -0300

    fix: Modificar descripcion valor
    Se modifica la descripcion del valor de la moneda.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
           <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
         <div>
-          Valor del Dólar Oficial: <b><a style={{ textDecoration: 'none', color: '#616161' }} data-bs-toggle="modal" href="#staticBackdrop" role="button">101.65</a></b>
+          Valor del <i>Dólar Oficial</i>: <b><a style={{ textDecoration: 'none', color: '#616161' }} data-bs-toggle="modal" href="#staticBackdrop" role="button">101.65</a></b>
         </div>
       </div>
       <hr />
